41f04fdcc9d90fa8c8c023325d07b742ef9d544f
leah <leah@launchbadge.com>
2021-05-11 12:47:43 -0700

    fist attempt at fixing issues

--- hedera/target/debug/deps/hedera-50ebaa1a7a1a5cd5.d
+++
@@ -1,7 +0,0 @@
-/home/leahwhalen/Documents/hedera-sdk-rust/hedera/target/debug/deps/hedera-50ebaa1a7a1a5cd5.rmeta: src/lib.rs
-
-/home/leahwhalen/Documents/hedera-sdk-rust/hedera/target/debug/deps/libhedera-50ebaa1a7a1a5cd5.rlib: src/lib.rs
-
-/home/leahwhalen/Documents/hedera-sdk-rust/hedera/target/debug/deps/hedera-50ebaa1a7a1a5cd5.d: src/lib.rs
-
-src/lib.rs:
--- hedera/target/debug/libhedera.d
+++
@@ -1,1 +0,0 @@
-/home/leahwhalen/Documents/hedera-sdk-rust/hedera/target/debug/libhedera.rlib: /home/leahwhalen/Documents/hedera-sdk-rust/hedera/src/lib.rs
