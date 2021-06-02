1963565c279df95114ac799c45238ad554ed28df
leah <leah@launchbadge.com>
2021-06-02 10:39:23 -0700

    update: saving work to rebase

--- hedera_crypto/src/lib.rs
+++ hedera_crypto/src/lib.rs
@@ -23,3 +23,32 @@ fn basics_perimeter() {
     dotest(20, 114624);
 }
  */
+
+use ed25519_dalek::Keypair;
+use bip39::Mnemonic;
+use rand::Rng;
+
+// Mnemonic phrase struct
+pub struct MnemonicWords {
+    props_words : Vec<String>,
+    props_legacy : bool,
+}
+
+impl MnemonicWords {
+    fn generate(length: i32) {
+        let mut needed_entropy = 0;
+
+        if length == 12 { needed_entropy = 16; }
+        else if length == 24 { needed_entropy = 32; }
+        else { /* error */ }
+
+        let seed = rand::thread_rng().gen::<[u8; needed_entropy]>();
+    }
+}
+
+//todo read(password) -> PrivateKey
+fn read( password : String ) -> String {
+    format!("PrivateKey")
+}
+
+//todo write(PrivateKey, password)
