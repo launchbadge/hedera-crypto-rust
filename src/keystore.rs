8d087f900cfcb99ecc519c360dd10dbcb2e7ac94
leah <leah@launchbadge.com>
2021-06-03 14:45:04 -0700

    feat: added the pbkdf2 algorithm to derive keys

--- src/keystore.rs
+++ src/keystore.rs
@@ -1,42 +1,29 @@
-//use keystore;
-use ed25519_dalek::Keypair;
-use hex;
-use pbkdf2;
+//use ed25519_dalek::Keypair;
+//use hex;
 use rand::Rng;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-    //fn test_keystore_read() { assert_eq!(keystore.read(""), format!("PrivateKey")); }
-}
+use ring::{digest, pbkdf2};
+use std::num::NonZeroU32;
 
 // constants
-const HMAC_SHA256 : String = "hmac-sha256";
+static PBKDF2_ALG: pbkdf2::Algorithm = pbkdf2::PBKDF2_HMAC_SHA256;
+const CREDENTIAL_LEN: usize = digest::SHA256_OUTPUT_LEN;
+pub type Credential = [u8; CREDENTIAL_LEN];
 
-//todo define special types
+pub struct KeyStore();
 
 // create keystore
+// returns JSON String of keystore?
 impl KeyStore {
+    pub async fn create_keystore(_private_key: &[u8], pass: &[u8]) {
+        let c_iter: NonZeroU32 = std::num::NonZeroU32::new(262144).unwrap();
+        let mut dk_len: Credential = [0u8; CREDENTIAL_LEN];
+        let salt = rand::thread_rng().gen::<[u8; 32]>();
 
-    pub async fn createKeystore(privateKey : u8, String : pass) {
-        const dkLen : i32 = 32;
-        const c : i32 = 262144;
-        const saltLen : i32 = 32;
-        let salt = rand::thread_rng().gen::<[u8; dkLen]>().await;
+        let key = ring::pbkdf2::derive(PBKDF2_ALG, c_iter, &salt, &pass, &mut dk_len);
 
-        let key = pbkdf2.deriveKey(
-            hmac.HashAlgorithm.Sha256,
-            passphrase,
-            salt,
-            c,
-            dkLen
-        ).await;
+        let iv = rand::thread_rng().gen::<[u8; 16]>();
 
-        let iv = rand::thread_rng().gen::<[u8; 16]>().await;
-
+        // creates cipher initialization vector
         // const cipherText = crypto.createCipheriv(
         //     crypto.CipherAlgorithm.Aes128Ctr,
         //     key.slice(0, 16),
@@ -49,7 +36,25 @@ impl KeyStore {
         //     key.slice(16),
         //     cipherText
         // );
+    }
+
+    //pub async fn derive_key(algorithm, password : &[u8], )
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::keystore::KeyStore;
+
+    #[test]
+    fn create_keystore() {
+        let private_key: &[u8] = &[
+            215, 90, 152, 1, 130, 177, 10, 183, 213, 75, 254, 211, 201, 100, 7, 58, 14, 225, 114,
+            243, 218, 166, 35, 37, 175, 2, 26, 104, 247, 7, 81, 26,
+        ];
+
+        //let keystore = KeyStore::create_keystore(private_key, format!("hello"));
+
+        //assert_eq!(&private_key.to_bytes(), keystore);
 
     }
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 mod key_error;
 mod private_key;
+mod keystore;
 mod public_key;
-pub use key_error::KeyError;
+pub use public_key::{KeyError, PublicKey};
 
 pub use private_key::PrivateKey;
 pub use public_key::PublicKey;
+
+pub use keystore::KeyStore;
+
