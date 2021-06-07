cb7d2b71ba3c92f85c4ea63f4b4c84ee0eda7fa8
rdsaechao <ricky@launchbadge.com>
2021-06-07 09:43:00 -0700

    feat: add private key

--- src/private_key.rs
+++ src/private_key.rs
@@ -1,7 +1,10 @@
 use ed25519_dalek::{
-    Keypair, PublicKey, SecretKey, Signature, Signer, SECRET_KEY_LENGTH, SIGNATURE_LENGTH,
+    ExpandedSecretKey, Keypair, PublicKey, SecretKey, Signature, Signer,
+    SECRET_KEY_LENGTH, SIGNATURE_LENGTH,
 };
 use rand::rngs::OsRng;
+use rand::RngCore;
+use rand_core::OsRng;
 use std::fmt;
 use std::fmt::{Display, Formatter};
 use std::str;
@@ -19,40 +22,36 @@ pub enum KeyError {
 
 #[derive(Debug)]
 /// A Private Key on the Hedera™ Network
-pub struct PrivateKey(SecretKey);
-
-/// A Public Key on the Hedera™ Network
-#[derive(Debug)]
-pub struct Public(PublicKey);
+pub struct PrivateKey(SecretKey, Option<[u8;32]>);
 
 impl PrivateKey {
     pub fn generate() -> PrivateKey {
         let mut csprng = OsRng {};
+        let mut array = [0u8; 16];
+        rand_core::OsRng.fill_bytes(&mut array);
+        let chain_code = rand_core::OsRng.next_32();
+
         let keypair = Keypair::generate(&mut csprng);
         //fix: should be alphanumeric
-        PrivateKey(keypair.secret)
+        PrivateKey(keypair.secret, chain_code)
     }
-    /// Returns a public key.
-    ///
-    /// # Arguments
-    ///
-    /// * `data` - An array of u8 with length of ed25519_dalek::PRIVATE_KEY_LENGTH.
-    ///
+
     pub fn from_bytes(data: &[u8]) -> Result<PrivateKey, KeyError> {
         let der_prefix_bytes = hex::decode("302e020100300506032b657004220420").unwrap();
         let private_key = match data.len() {
             32 => {
                 let private_key =
-                    PrivateKey(SecretKey::from_bytes(&data).map_err(KeyError::Signature)?);
+                    PrivateKey(SecretKey::from_bytes(&data).map_err(KeyError::Signature)?, None);
                 private_key
             }
             48 if data.starts_with(&der_prefix_bytes) => {
                 let private_key =
-                    PrivateKey(SecretKey::from_bytes(&data[..16]).map_err(KeyError::Signature)?);
+                    PrivateKey(SecretKey::from_bytes(&data[16..47]).map_err(KeyError::Signature)?, None);
+
                 private_key
             }
             64 => {
-                let private_key = PrivateKey(SecretKey::from_bytes(&data[..SECRET_KEY_LENGTH])?);
+                let private_key = PrivateKey(SecretKey::from_bytes(&data[..SECRET_KEY_LENGTH])?, None);
                 private_key
             }
             _ => {
@@ -61,28 +60,18 @@ impl PrivateKey {
         };
         Ok(private_key)
     }
-    /// Return an array of u8 with length of ed25519_dalek::SECRET_KEY_LENGTH.
-    ///
-    /// # Arguments
-    ///
-    /// * `Secret_key` - ed25519_dalek::SecretKey
-    ///
+
     pub fn to_bytes(&self) -> [u8; SECRET_KEY_LENGTH] {
         self.0.to_bytes()
     }
 
-    // Sign a
-    //
-    // # Arguments
-    //
-    // * `Secret_key` - ed25519_dalek::SecretKey
-    //
-    // pub fn sign(&self, message: &[u8]) ->  [u8; SIGNATURE_LENGTH]{
-    //     let
-    //     let signature: Signature = self.sign(message);
-    //     let signature_bytes:  [u8; SIGNATURE_LENGTH]  = signature.to_bytes();
-    //     signature_bytes
-    // }
+    pub fn sign(&self, message: &[u8]) -> [u8; SIGNATURE_LENGTH] {
+        let public_key: PublicKey = (&self.0).into();
+        let expanded_private = ExpandedSecretKey::from(&self.0);
+        let signature: Signature = expanded_private.sign(message, &public_key);
+        let signature_bytes: [u8; SIGNATURE_LENGTH] = signature.to_bytes();
+        signature_bytes
+    }
 
     pub fn public_key(&self) -> PublicKey {
         let public_key: PublicKey = (&self.0).into();
@@ -117,10 +106,10 @@ impl FromStr for PrivateKey {
 
 #[cfg(test)]
 mod tests {
-    use super::{KeyError, Keypair, PrivateKey};
-    use crate::private_key::DER_PREFIX;
+    use super::{KeyError, Keypair, PrivateKey, Signature, Signer, SIGNATURE_LENGTH};
     use ed25519_dalek::{SecretKey, SECRET_KEY_LENGTH};
-    use rand::rngs::OsRng;
+    use rand::{rngs::OsRng, RngCore};
+    use rand_core::{OsRng};
     use std::str::FromStr;
 
     const PRIVATE_KEY_BYTES: &[u8; SECRET_KEY_LENGTH] = &[
@@ -129,11 +118,26 @@ mod tests {
     ];
 
     #[test]
-    fn test_from_bytes() -> Result<(), KeyError> {
-        let private_key = PrivateKey::from_bytes(PRIVATE_KEY_BYTES)?;
+    fn test_from_bytes_32() -> Result<(), KeyError> {
+        let private_key = PrivateKey(SecretKey::from_bytes(PRIVATE_KEY_BYTES)?, None);
 
         assert_eq!(&private_key.to_bytes(), PRIVATE_KEY_BYTES);
 
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_bytes_48() -> Result<(), KeyError> {
+        let string= "302e020100300506032b6570042204209d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60";
+        let no_der_str = "9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60";
+
+        let private_bytes = string.as_bytes();
+        let secret_bytes = no_der_str.as_bytes();
+
+        let private_key = PrivateKey::from_bytes(private_bytes)?;
+        let secret_key = PrivateKey::from_bytes(secret_bytes)?;
+
+        assert_eq!(private_key, secret_key);
         Ok(())
     }
 
@@ -155,22 +159,48 @@ mod tests {
         let private_from_bytes = PrivateKey::from_bytes(&secret_to_bytes);
         let private_to_bytes = PrivateKey::to_bytes(&private_from_bytes?);
         let private_from_bytes_2 = PrivateKey::from_bytes(&private_to_bytes);
-        let public_key = PrivateKey::public_key(&private_from_bytes_2?);
 
-        assert_eq!(public_key, keypair.public);
+        assert_eq!(
+            PrivateKey::public_key(&private_from_bytes_2?),
+            keypair.public
+        );
 
         Ok(())
     }
 
     #[test]
     fn test_from_str() -> Result<(), KeyError> {
-        let private_key = PrivateKey::generate();
+        let private_key = PrivateKey::from_bytes(PRIVATE_KEY_BYTES)?;
 
         let key_string = private_key.to_string();
 
-        let private_from_str = PrivateKey::from_str(&key_string)?;
+        assert_eq!(PrivateKey::from_str(&key_string)?, private_key);
 
-        assert_eq!(private_from_str, private_key);
+        Ok(())
+    }
+
+    #[test]
+    fn test_to_string() -> Result<(), KeyError> {
+        let private_key = PrivateKey::from_bytes(PRIVATE_KEY_BYTES)?;
+        let private_str= "302e020100300506032b6570042204209d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60";
+
+        assert_eq!(format!("{}", private_key), private_key.to_string());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_sign() -> Result<(), KeyError> {
+        let mut csprng = rand::rngs::OsRng {};
+        let keypair: Keypair = Keypair::generate(&mut csprng);
+        let message: &[u8] = b"This is a test";
+        let signature: Signature = keypair.sign(message);
+        let signature_bytes: [u8; SIGNATURE_LENGTH] = signature.to_bytes();
+
+        assert_eq!(
+            PrivateKey::sign(&PrivateKey(keypair.secret, None), message),
+            signature_bytes
+        );
 
         Ok(())
     }
