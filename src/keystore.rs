c0d1e9e9a1944bc53710be26d50d9bd39d874b2f
leah <leah@launchbadge.com>
2021-06-07 17:22:09 -0700

    feat: keystore added, needs to be tested

--- src/keystore.rs
+++ src/keystore.rs
@@ -1,72 +1,87 @@
-use aes::Aes128;
-use block_modes::block_padding::Pkcs7;
-use block_modes::{BlockMode, Cbc};
-use rand::Rng;
-use ring::{digest, pbkdf2};
-use std::num::NonZeroU32;
-use sha2::Sha384;
+use ctr::cipher::{NewCipher, StreamCipher};
 use hmac::{Hmac, Mac, NewMac};
+use pbkdf2;
+use rand::Rng;
+use sha2::Sha384;
+use serde_json;
 
-// to generate key
-static PBKDF2_ALG: pbkdf2::Algorithm = pbkdf2::PBKDF2_HMAC_SHA256;
-const CREDENTIAL_LEN: usize = digest::SHA256_OUTPUT_LEN;
-pub type Credential = [u8; CREDENTIAL_LEN];
-
-// to generate cipher text
-type Aes128Cbc = Cbc<Aes128, Pkcs7>;
+// CTR mode implementation is generic over block ciphers
+// we will create a type alias for convenience
+type Aes128Ctr = ctr::Ctr128BE<aes::Aes128>;
 
 // Create alias for HMAC-SHA256
 type HmacSha384 = Hmac<Sha384>;
 
-pub struct KeyStore();
+#[derive(serde::Serialize)]
+pub struct KDFParams {
+    dk_len: i32,
+    salt: [u8; 32],
+    c: i32,
+    prf: String,
+}
+
+#[derive(serde::Serialize)]
+pub struct Crypto {
+    cipher_text: [u8; 32],
+    cipher_params: [u8; 16],
+    cipher: String,
+    kdf: String,
+    kdf_params: KDFParams,
+}
+
+#[derive(serde::Serialize)]
+pub struct KeyStore {
+    version: i32,
+    crypto: Crypto,
+    mac: Vec<u8>,
+}
 
 // create keystore
 //      returns JSON String of keystore?
 impl KeyStore {
-    pub async fn create_keystore(private_key: &[u8], pass: &[u8]) {
-        let c_iter: NonZeroU32 = std::num::NonZeroU32::new(262144).unwrap();
-        let mut derived_key: Credential = [0u8; CREDENTIAL_LEN];
+    pub fn create_keystore(private_key: &[u8], pass: &[u8]) -> String {
+        let c_iter: u32 = 262144;
+        let mut derived_key: [u8; 32] = [0; 32];
         let salt = rand::thread_rng().gen::<[u8; 32]>();
-
-        ring::pbkdf2::derive(PBKDF2_ALG, c_iter, &salt, &pass, &mut derived_key);
-
         let iv = rand::thread_rng().gen::<[u8; 16]>();
 
+        pbkdf2::pbkdf2::<HmacSha384>(pass, &salt, c_iter, &mut derived_key);
+
         // AES-128-CTR with the first half of the derived key and a random IV
-        let cipher = Aes128Cbc::new_from_slices(&derived_key[0..16], &iv).unwrap();
+        let mut cipher = Aes128Ctr::new_from_slices(&derived_key[0..16], &iv).unwrap();
         let mut buffer = [0u8; 32];
 
         // copy message to the buffer
         let pos = private_key.len();
         buffer[..pos].copy_from_slice(private_key);
-        let cipher_text = cipher.encrypt(&mut buffer, pos).unwrap();
+        // let cipher_text = cipher.encrypt(&mut buffer, pos).unwrap();
+        cipher.apply_keystream(&mut buffer);
 
-        let mut mac = HmacSha384::new_from_slice(&derived_key[16 .. derived_key.len()])
+        let mut mac = HmacSha384::new_from_slice(&derived_key[16..derived_key.len()])
             .expect("HMAC can take key of any size");
-        mac.update(cipher_text);
+        mac.update(&buffer);
 
         // get auth code in bytes
         let result = mac.finalize();
         let code_bytes = result.into_bytes();
 
-
-        // const keystore = {
-        //     version: 1,
-        //     crypto: {
-        //         ciphertext: hex.encode(cipherText),
-        //         cipherparams: { iv: hex.encode(iv) },
-        //         cipher: crypto.CipherAlgorithm.Aes128Ctr,
-        //         kdf: "pbkdf2",
-        //         kdfparams: {
-        //             dkLen,
-        //             salt: hex.encode(salt),
-        //             c,
-        //             prf: HMAC_SHA256,
-        //         },
-        //         mac: hex.encode(mac),
-        //     },
-        // };
-
+        let keystore = KeyStore {
+            version: 1,
+            crypto: Crypto {
+                cipher_text: buffer,
+                cipher_params: iv,
+                cipher: "AES-128-CTR".to_string(),
+                kdf: "pbkdf2".to_string(),
+                kdf_params: KDFParams {
+                    dk_len: 32,
+                    salt: salt,
+                    c: 262144,
+                    prf: "hmac-sha256".to_string(),
+                },
+            },
+            mac: (&*code_bytes).to_vec(),
+        };
+        serde_json::to_string(&keystore).unwrap()
     }
 }
 
@@ -77,8 +92,11 @@ mod tests {
     #[test]
     fn create_keystore() {
         let private_key: &[u8] = &[
-            215, 90, 152, 1, 130, 177, 10, 183, 213, 75, 254, 211, 201, 100, 7, 58, 14, 225, 114,
-            243, 218, 166, 35, 37, 175, 2, 26, 104, 247, 7, 81, 26,
+            -37 as u8, 72 as u8, 75 as u8, -126 as u8, -114 as u8, 100 as u8, -78 as u8, -40 as u8,
+            -15 as u8, 44 as u8, -29 as u8, -64 as u8, -96 as u8, -23 as u8, 58 as u8, 11 as u8,
+            -116 as u8, -50 as u8, 122 as u8, -15 as u8, -69 as u8, -113 as u8, 57 as u8,
+            -55 as u8, 119 as u8, 50 as u8, 57 as u8, 68 as u8, -126 as u8, 83 as u8, -114 as u8,
+            16 as u8,
         ];
 
         //let keystore = KeyStore::create_keystore(private_key, format!("hello"));
