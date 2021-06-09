fa8c0881a9594784ac2ec1deeae11cca46adba78
leah <leah@launchbadge.com>
2021-06-09 15:47:47 -0700

    feat(WIP): added the create_key_list function for KeyList

--- src/key_list.rs
+++ src/key_list.rs
@@ -1,12 +1,20 @@
 use crate::key::Key;
 
 pub struct KeyList {
-    keys: [Key],
-    threshold: i32,
+    keys: Vec<Key>,
+    threshold: Result<i32, E>,
 }
 
-// todo: of(keys: vec[Key]) -> KeyList
+impl KeyList {
+    // todo: of(keys: Vec<Key>) -> KeyList
+    pub fn create_key_list(keys: Vec<Key>, threshold: Result<i32, E>) -> KeyList {
+        KeyList{
+            keys,
+            threshold: threshold,
+        }
+    }
 
-// todo: from(keys: vec[Key], mapFn: , T) -> KeyList
+    // todo: from(keys: Vec<Key>, mapFn: ?, T) -> KeyList
 
-// todo:
+    // todo: push(keys: Vec<Key>) -> i32
+}
