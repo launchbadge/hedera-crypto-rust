efe53e57d8e8e1ba0bc2b01cf8cb903e71351818
Ricky Saechao <ricky@launchbadge.com>
2021-06-11 17:21:28 -0700

    feat: add private key and test

--- src/private_key.rs
+++ src/private_key.rs
@@ -10,13 +10,15 @@ use std::str;
 use std::str::FromStr;
 
 const DER_PREFIX: &str = "302e020100300506032b657004220420";
+const DER_PREFIX_BYTES: &[u8] = &[48, 46, 2, 1, 0, 48, 5, 6, 3, 43, 101, 112, 4, 34, 4, 32];
 
-#[derive(Debug)]
 /// A private key on the Hedera™ Network
+#[derive(Debug)]
 pub struct PrivateKey {
     keypair: Keypair,
     chain_code: Option<[u8; 32]>,
 }
+
 impl PrivateKey {
     pub fn generate() -> PrivateKey {
         let mut entropy = [0u8; 64];
@@ -33,57 +35,55 @@ impl PrivateKey {
     }
 
     pub fn from_bytes(data: &[u8]) -> Result<PrivateKey, KeyError> {
-        let der_prefix_bytes = hex::decode("302e020100300506032b657004220420").unwrap();
-        let private_key = match data.len() {
+        Ok(match data.len() {
             32 => {
                 let secret_key = SecretKey::from_bytes(&data).map_err(KeyError::Signature)?;
-                let private_key = PrivateKey {
+                PrivateKey {
                     keypair: Keypair {
                         public: (&secret_key).into(),
                         secret: secret_key,
                     },
                     chain_code: None,
-                };
-                private_key
+                }
             }
-            48 if data.starts_with(&der_prefix_bytes) => {
+
+            48 if data.starts_with(&DER_PREFIX_BYTES) => {
                 let secret_key = SecretKey::from_bytes(&data[16..]).map_err(KeyError::Signature)?;
-                let private_key = PrivateKey {
+                PrivateKey {
                     keypair: Keypair {
                         public: (&secret_key).into(),
                         secret: secret_key,
                     },
                     chain_code: None,
-                };
-                private_key
+                }
             }
+
             64 => {
                 let secret_key = SecretKey::from_bytes(&data[..SECRET_KEY_LENGTH])
                     .map_err(KeyError::Signature)?;
-                let private_key = PrivateKey {
+                PrivateKey {
                     keypair: Keypair {
                         public: (&secret_key).into(),
                         secret: secret_key,
                     },
                     chain_code: None,
-                };
-                private_key
+                }
             }
+
             _ => {
                 return Err(KeyError::Length(data.len()));
             }
-        };
-        Ok(private_key)
+        })
     }
+
     pub fn to_bytes(&self) -> [u8; SECRET_KEY_LENGTH] {
         self.keypair.secret.to_bytes()
     }
 
     pub fn sign(&self, message: &[u8]) -> [u8; SIGNATURE_LENGTH] {
-        let signature: Signature = self.keypair.sign(message);
-        let signature_bytes: [u8; SIGNATURE_LENGTH] = signature.to_bytes();
-        signature_bytes
+        self.keypair.sign(message).to_bytes()
     }
+
     pub fn public_key(&self) -> PublicKey {
         self.keypair.public
     }
@@ -94,7 +94,9 @@ impl PartialEq for PrivateKey {
         self.keypair.secret.to_bytes() == other.keypair.secret.to_bytes()
     }
 }
+
 impl Eq for PrivateKey {}
+
 impl Display for PrivateKey {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(
@@ -105,12 +107,11 @@ impl Display for PrivateKey {
         )
     }
 }
+
 impl FromStr for PrivateKey {
     type Err = KeyError;
     fn from_str(text: &str) -> Result<Self, KeyError> {
-        let decoded_private_key = hex::decode(&text).unwrap();
-        let private_key = PrivateKey::from_bytes(&decoded_private_key)?;
-        Ok(private_key)
+        Ok(PrivateKey::from_bytes(&hex::decode(&text).unwrap())?)
     }
 }
 
@@ -125,18 +126,27 @@ mod tests {
     #[test]
     fn test_generate() -> Result<(), KeyError> {
         let private_key = PrivateKey::generate();
+
         assert_eq!(private_key.keypair.secret.to_bytes().len(), 32 as usize);
+
         Ok(())
     }
+
     #[test]
     fn test_from_bytes() -> Result<(), KeyError> {
-        let private_key = PrivateKey::from_str(PRIVATE_KEY_STR)?;
         assert_eq!(
-            PrivateKey::from_bytes(&private_key.keypair.secret.to_bytes())?,
-            private_key
+            PrivateKey::from_bytes(
+                &PrivateKey::from_str(PRIVATE_KEY_STR)?
+                    .keypair
+                    .secret
+                    .to_bytes()
+            )?,
+            PrivateKey::from_str(PRIVATE_KEY_STR)?
         );
+
         Ok(())
     }
+
     #[test]
     fn test_to_bytes() -> Result<(), KeyError> {
         let private_key = PrivateKey::from_str(PRIVATE_KEY_STR)?;
@@ -144,8 +154,10 @@ mod tests {
             &PrivateKey::to_bytes(&private_key),
             &private_key.keypair.secret.to_bytes()
         );
+
         Ok(())
     }
+
     #[test]
     fn test_public_key() -> Result<(), KeyError> {
         let private_key = PrivateKey::from_str(PRIVATE_KEY_STR)?;
@@ -154,28 +166,20 @@ mod tests {
             PrivateKey::public_key(&private_key),
             private_key.keypair.public
         );
+
         Ok(())
     }
+
     #[test]
-    fn test_from_str() -> Result<(), KeyError> {
-        let private_key = PrivateKey::from_str(PRIVATE_KEY_STR)?;
-        let key_string = private_key.to_string();
+    fn test_to_from_string() -> Result<(), KeyError> {
         assert_eq!(
-            PrivateKey::from_str(&key_string)?.to_string(),
-            private_key.to_string()
+            PrivateKey::from_str(PRIVATE_KEY_STR)?.to_string(),
+            PRIVATE_KEY_STR
         );
+
         Ok(())
     }
-    #[test]
-    fn test_to_string() -> Result<(), KeyError> {
-        let private_key = PrivateKey::from_str(PRIVATE_KEY_STR)?;
-        let key_string = private_key.to_string();
-        assert_eq!(
-            PrivateKey::from_str(&key_string)?.to_string(),
-            private_key.to_string()
-        );
-        Ok(())
-    }
+
     #[test]
     fn test_sign() -> Result<(), KeyError> {
         let mut entropy = [0u8; 64];
@@ -184,7 +188,9 @@ mod tests {
         let message: &[u8] = b"This is a test";
         let signature: Signature = key.keypair.sign(message);
         let signature_bytes: [u8; SIGNATURE_LENGTH] = signature.to_bytes();
+
         assert_eq!(PrivateKey::sign(&key, message), signature_bytes);
+
         Ok(())
     }
 }
