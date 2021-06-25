2b6fd941e5136f737ca7f54b6f3b3ccfb93e7d4d
leah <leah@launchbadge.com>
2021-06-25 11:57:30 -0700

    feat: completed all methods. still testing

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ mod key_error;
 mod private_key;
 mod public_key;
 pub use key_error::KeyError;
-mod key_list;
 mod key;
+mod key_list;
 
 pub use private_key::PrivateKey;
 pub use public_key::PublicKey;
