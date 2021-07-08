d52679c650d3a446e498bf24549bd83389a35899
David Lindner <davidlindner@pop-os.localdomain>
2021-07-08 15:10:09 -0700

    Final fixes for entropy file

--- examples/generate_key.rs
+++ examples/generate_key.rs
@@ -1,36 +1,31 @@
 use std::str::FromStr;
 
-use hedera_crypto::{Mnemonic, PrivateKey};
+use hedera_crypto::Mnemonic;
 
 fn main() -> anyhow::Result<()> {
     // generate a 24-word mnemonic
-    let keystore_1 = std::fs::read("./keystore.bin")?;
-    let key = PrivateKey::from_keystore(&keystore_1, "")?;
+    let mnemonic = Mnemonic::generate(24)?;
 
     // convert to a root key
-    // let root_key = mnemonic.to_private_key("")?;
+    let root_key = mnemonic.to_private_key("")?;
 
     // derive index #0
     // WARN: don't hand out your root key
-    // let key = root_key.derive(0)?;
+    let key = root_key.derive(0)?;
 
     println!("private key = {}", key);
     println!("public key = {}", key.public_key());
-
-    let keystore_2 = key.to_keystore("")?;
-
-    std::fs::write("keystore2.bin", keystore_2)?;
 
     // [...]
 
     // recover your key from the mnemonic
     // this takes space-separated or comma-separated words
 
-    // let recovered_mnemonic = Mnemonic::from_str(&mnemonic.to_string())?;
-    // let recovered_root_key = recovered_mnemonic.to_private_key("")?;
-    // let recovered_key = recovered_root_key.derive(0)?;
+    let recovered_mnemonic = Mnemonic::from_str(&mnemonic.to_string())?;
+    let recovered_root_key = recovered_mnemonic.to_private_key("")?;
+    let recovered_key = recovered_root_key.derive(0)?;
 
-    // assert_eq!(recovered_key, key);
+    assert_eq!(recovered_key, key);
 
     Ok(())
 }
--- src/derive.rs
+++ src/derive.rs
@@ -13,7 +13,6 @@ pub(crate) fn legacy(seed: &[u8; 32], index: i32) -> [u8; 32] {
 
     BUF.with(|buf| {
         let mut buf = buf.borrow_mut();
-
         buf.copy_from_slice(&seed[..]);
 
         // FIXME: when legacy derive is fully fixed this code should be re-ported
@@ -26,26 +25,4 @@ pub(crate) fn legacy(seed: &[u8; 32], index: i32) -> [u8; 32] {
 
         derived_key
     })
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    const TEST_SEED: &[u8; 32] = &[
-        157, 097, 177, 157, 239, 253, 090, 096, 186, 132, 074, 244, 146, 236, 044, 196, 068, 073,
-        197, 105, 123, 050, 105, 025, 112, 059, 172, 003, 028, 174, 127, 096,
-    ];
-
-    #[test]
-    fn test_legacy() {
-        let legacy = legacy(TEST_SEED, 0);
-        assert_eq!(
-            legacy,
-            [
-                60, 129, 234, 118, 66, 190, 0, 29, 70, 223, 185, 180, 215, 16, 148, 77, 104, 37,
-                174, 169, 101, 52, 64, 84, 119, 252, 186, 208, 89, 214, 35, 172
-            ]
-        );
-    }
 }
